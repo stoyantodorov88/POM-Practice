34e8f2bd8b7e71bd02f991a3003dc065799851de
stoyantodorov88 <stoyantodorov88@gmail.com>
2020-08-18 20:31:23 +0300

    add file with change

--- UserFactory.cs
+++ UserFactory.cs
@@ -19,7 +19,7 @@ namespace AutomationPractice
                 FirstName = fixture.Create<string>(),
                 LastName = fixture.Create<string>(),
                 Email = fixture.Create<string>(),
-                Gender = "Neutral",
+                Gender = "Neutral again",
                 Phone = fixture.Create<int>(),
                 Day = dateTime.Date.ToString(),
                 Month = dateTime.Month.ToString(),
