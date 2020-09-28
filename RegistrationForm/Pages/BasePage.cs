1bd950199a79252c12825b10230f0b9637653462
stoyantodorov88 <stoyantodorov88@gmail.com>
2020-09-28 21:29:42 +0300

    Practice

---
+++ RegistrationForm/Pages/BasePage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationPractice
+{
+    public class BasePage
+    {
+               
+            public IWebDriver Driver { get; }
+
+            public WebDriverWait Wait { get; }
+
+            public BasePage(IWebDriver driver)
+            {
+
+                Driver = driver;
+                Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(4));
+              
+            }
+
+
+        public IWebElement Scroll(IWebElement element) 
+        {
+            ((IJavaScriptExecutor)Driver).ExecuteScript("arguments[0].scrollIntoView(true);", element);
+            return element;
+        }
+    }
+}
